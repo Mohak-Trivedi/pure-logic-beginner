5e5dd32a5bfc9025b16d89281374f59b3e7abada
Mohak-Trivedi <trivedimohak@gmail.com>
2023-08-31 23:40:50 +0530

    alternate approach for Day Of The Programmer

--- DayOfTheProgrammer.java
+++ DayOfTheProgrammer.java
@@ -21,29 +21,64 @@ class Result {
      * The function accepts INTEGER year as parameter.
      */
 
+    // Approach: Involves manual calculation
+    // public static String dayOfProgrammer(int year) {
+    // // Write your code here
+    // String dayOfProgrammer = "";
+    // String ddmm = "";
+    // if (year < 1918) {
+    // // Julian Calender
+    // // Programmer Day = (if leap year: 366 days) -> 12th September (Programmer's
+    // Day
+    // // = 256th day: 244 till Sept + 12 from Sept)
+    // // Programmer Day = +1 day (non-leap year: 365 days: 1 day less in Feb, so +1
+    // // day to include from Sept) -> 13th September
+    // ddmm = (year % 4 == 0) ? "12.09." : "13.09.";
+    // } else if (year > 1918) {
+    // // Gregorian Calender
+    // // Same as Julian, but Leap Year considered in different manner
+    // ddmm = ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) ?
+    // "12.09." : "13.09.";
+    // } else {
+    // // 1918: Shifting from Julian to Gregorian, and is Non-Leap
+    // // Lost 13 days from Feb, so +13 in Sept Non-Leap. Programmer Day: 26th
+    // // September
+    // ddmm = "26.09.";
+    // }
+    // dayOfProgrammer = ddmm + year;
+    // return dayOfProgrammer;
+    // }
+
+    // Alternate Approach: Less manual calculation
     public static String dayOfProgrammer(int year) {
-        // Write your code here
-        String dayOfProgrammer = "";
-        String ddmm = "";
+        int[] daysOfMonths = new int[] { 31, 28, 31, 30, 31, 30, 31, 31 }; // Non-leap : Jan-Aug : Not including 30 of
+                                                                           // Sept as it will cross sum 256 i.e.
+                                                                           // Programmer Day
+        int programmerDay = 256;
+        int daysTillSept = 0, daysOfSept = 0;
+
         if (year < 1918) {
             // Julian Calender
-            // Programmer Day = (if leap year: 366 days) -> 12th September (Programmer's Day
-            // = 256th day: 244 till Sept + 12 from Sept)
-            // Programmer Day = +1 day (non-leap year: 365 days: 1 day less in Feb, so +1
-            // day to include from Sept) -> 13th September
-            ddmm = (year % 4 == 0) ? "12.09." : "13.09.";
+            if (year % 4 == 0) {
+                daysOfMonths[1] = 29; // 29 days in Feb in leap year
+            } // else it will be as it is i.e. 28 days
         } else if (year > 1918) {
             // Gregorian Calender
             // Same as Julian, but Leap Year considered in different manner
-            ddmm = ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) ? "12.09." : "13.09.";
+            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+                daysOfMonths[1] = 29;
+            }
         } else {
             // 1918: Shifting from Julian to Gregorian, and is Non-Leap
             // Lost 13 days from Feb, so +13 in Sept Non-Leap. Programmer Day: 26th
             // September
-            ddmm = "26.09.";
+            return "26.09." + year;
         }
-        dayOfProgrammer = ddmm + year;
-        return dayOfProgrammer;
+        for (int days : daysOfMonths) {
+            daysTillSept += days;
+        }
+        daysOfSept = programmerDay - daysTillSept; // remaining days will be included from Sept
+        return Integer.toString(daysOfSept) + ".09." + Integer.toString(year);
     }
 
 }
