4e7086c4535bafd3fc8f0c79db37c66355fdf559
Mohak-Trivedi <trivedimohak@gmail.com>
2023-09-02 01:46:42 +0530

    Solved viral advertising

---
+++ ViralAdvertising.java
@@ -0,0 +1,54 @@
+// Problem: https://www.hackerrank.com/challenges/strange-advertising/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+        // Write your code here
+        int totalLikes = 0;
+        int shared = 5;
+        for (int day = 1; day <= n; day++) {
+            int currDayLikes = shared / 2;
+            totalLikes += currDayLikes;
+            shared = currDayLikes * 3;
+        }
+        return totalLikes;
+    }
+
+}
+
+// Use class name Solution in HackerRank
+public class ViralAdvertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
