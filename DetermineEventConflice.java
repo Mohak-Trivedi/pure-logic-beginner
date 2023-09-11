bf9e70b62d8dfcde10cfc58042b8688776a805e2
Mohak-Trivedi <trivedimohak@gmail.com>
2023-09-12 00:47:23 +0530

    corrected comments

--- DetermineEventConflice.java
+++ DetermineEventConflice.java
@@ -24,7 +24,7 @@ class Solution {
     // }
     // }
 
-    // Alternate approach
+    // Alternate approach: passes all test cases
     public boolean haveConflict(String[] event1, String[] event2) {
         // Bring all times to same unit by converting each into minutes
         int event1_start = Integer.parseInt(event1[0].substring(0, 2)) * 60 + Integer.parseInt(event1[0].substring(3));
@@ -32,7 +32,7 @@ class Solution {
         int event2_start = Integer.parseInt(event2[0].substring(0, 2)) * 60 + Integer.parseInt(event2[0].substring(3));
         int event2_end = Integer.parseInt(event2[1].substring(0, 2)) * 60 + Integer.parseInt(event2[1].substring(3));
 
-        // The only 2 ways to avoid conflict: i.e. returing false:
+        // The only 2 ways to avoid conflict: i.e. returning false:
         // 1. event1_start <= event1_end < event2_start <= event2_end i.e. event1_end <
         // event2_start
         // 2. event2_start <= event2_end < event1_start <= event1_end i.e. event2_end <
